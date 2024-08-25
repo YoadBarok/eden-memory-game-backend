360b3a85b3e8e445796bf5ddc25d8610932c8d73
YoadBarok <yoad.barok@statsperform.com>
2024-08-25 20:26:34 +0200

    fix: allow cors only from FE

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const corsOptions: CorsOptions = {
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST'],
+  };
+  app.enableCors(corsOptions);
   await app.listen(8080);
 }
 bootstrap();
