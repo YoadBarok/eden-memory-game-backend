271e2cfeeef6c9537449af47d298b512fb3d8637
YoadBarok <yoad.barok@statsperform.com>
2024-08-25 20:52:29 +0200

    fix: remove un-needed .use invokation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-import { ApiKeyMiddleware } from './api-key/api-key.middleware';
-import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,7 +9,6 @@ async function bootstrap() {
     methods: ['GET', 'POST'],
   };
   app.enableCors(corsOptions);
-  app.use(new ApiKeyMiddleware(new PrismaService()));
   await app.listen(8080);
 }
 bootstrap();
